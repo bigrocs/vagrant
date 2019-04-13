77256b4728e09c771536cd15190b26d060439da9
bigrocs <bigrocs@qq.com>
2019-04-13 09:03:20 +0800

    增加启动box

--- rancher/start.sh
+++ rancher/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # 进入当前工作目录
+# chmod +x /root/code/vagrant/rancher/start.sh
 cd `dirname $0`
 vagrant up
---
+++ rancher/startbox.sh
@@ -0,0 +1,15 @@
+VBoxManage modifyvm server --autostart-enabled on
+
+VBoxManage modifyvm master-01 --autostart-enabled on
+VBoxManage modifyvm master-02 --autostart-enabled on
+VBoxManage modifyvm master-03 --autostart-enabled on
+
+VBoxManage modifyvm node-01 --autostart-enabled on
+VBoxManage modifyvm node-02 --autostart-enabled on
+VBoxManage modifyvm node-03 --autostart-enabled on
+VBoxManage modifyvm node-04 --autostart-enabled on
+VBoxManage modifyvm node-05 --autostart-enabled on
+VBoxManage modifyvm node-06 --autostart-enabled on
+VBoxManage modifyvm node-07 --autostart-enabled on
+VBoxManage modifyvm node-08 --autostart-enabled on
+VBoxManage modifyvm node-09 --autostart-enabled on
