924adcb0fc376517eabbbd5dad508f7c4ef31619
bigrocs <bigrocs@qq.com>
2019-04-13 15:41:29 +0800

    ..

--- rancher/start.sh
+++ rancher/start.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
-#chkconfig:2345 81 96
-#description:开机自启脚本
-cd /root/code/vagrant/rancher
-vagrant up
+vboxmanage startvm server --type headless
+
+vboxmanage startvm master-01 --type headless
+vboxmanage startvm master-02 --type headless
+vboxmanage startvm master-03 --type headless
+
+vboxmanage startvm node-01 --type headless
+vboxmanage startvm node-02 --type headless
+vboxmanage startvm node-03 --type headless
+vboxmanage startvm node-04 --type headless
+vboxmanage startvm node-05 --type headless
+vboxmanage startvm node-06 --type headless
+vboxmanage startvm node-07 --type headless
+vboxmanage startvm node-08 --type headless
+vboxmanage startvm node-09 --type headless
