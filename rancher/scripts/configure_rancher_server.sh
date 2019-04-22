40de474a428c7502ea9a0e2580edfa5ae157edf7
bigrocs <bigrocs@qq.com>
2019-04-22 09:33:05 +0800

    .

--- rancher/scripts/configure_rancher_server.sh
+++ rancher/scripts/configure_rancher_server.sh
@@ -57,86 +57,86 @@ SERVERURLRESPONSE=$(docker run --net host \
      -s 'https://127.0.0.1/v3/settings/server-url' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" -X PUT --data-binary '{"name":"server-url","value":"https://'$rancher_server_ip'"}' --insecure)
 
 # Create cluster
-CLUSTERRESPONSE=$(docker run --net host \
-    --rm \
-    $curl_prefix/curl \
-     -s 'https://127.0.0.1/v3/cluster' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" \
-     --data-binary "{\"type\":\"cluster\",\"rancherKubernetesEngineConfig\":{\"ignoreDockerVersion\":false,\"sshAgentAuth\":false,\"type\":\"rancherKubernetesEngineConfig\",\"kubernetesVersion\":\"$kubernetes_version\",\"authentication\":{\"type\":\"authnConfig\",\"strategy\":\"x509\"},\"network\":{\"type\":\"networkConfig\",\"plugin\":\"flannel\",\"flannelNetworkProvider\":{\"iface\":\"eth1\"},\"calicoNetworkProvider\":null},\"ingress\":{\"type\":\"ingressConfig\",\"provider\":\"nginx\"},\"services\":{\"type\":\"rkeConfigServices\",\"kubeApi\":{\"podSecurityPolicy\":false,\"type\":\"kubeAPIService\"},\"etcd\":{\"type\":\"etcdService\",\"extraArgs\":{\"heartbeat-interval\":500,\"election-timeout\":5000}}}},\"name\":\"$default_cluster\"}" --insecure)
-
 # CLUSTERRESPONSE=$(docker run --net host \
 #     --rm \
 #     $curl_prefix/curl \
 #      -s 'https://127.0.0.1/v3/cluster' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" \
-#      --data-binary '{
-#          "type":"cluster",
-#          "name":"'$default_cluster'",
-#          "dockerRootDir":"/var/lib/docker",
-#          "enableClusterAlerting":false,
-#          "enableClusterMonitoring":false,
-#          "enableNetworkPolicy":false,
-#          "localClusterAuthEndpoint":{
-#              "enabled":true,
-#              "type":"localClusterAuthEndpoint"
-#          },
-#          "rancherKubernetesEngineConfig":{
-#              "addonJobTimeout":30,
-#              "authentication":{
-#                  "strategy":"x509",
-#                  "type":"authnConfig"
-#              },
-#              "ignoreDockerVersion":true,
-#              "ingress":{
-#                  "provider":"nginx",
-#                  "type":"ingressConfig"
-#              },
-#              "kubernetesVersion":"'$kubernetes_version'",
-#              "monitoring":{
-#                  "provider":"metrics-server",
-#                  "type":"monitoringConfig"
-#              },
-#              "network":{
-#                  "options":{
-#                      "flannel_backend_type":"vxlan"
-#                  },
-#                  "plugin":"canal",
-#                  "type":"networkConfig"
-#              },
-#              "privateRegistries":[
-#                  {
-#                      "isDefault":true,"type":
-#                      "privateRegistry",
-#                      "url":""
-#                  }
-#              ],
-#              "services":{
-#                  "etcd":{
-#                      "backupConfig":{
-#                          "enabled":true,
-#                          "intervalHours":12,
-#                          "retention":6,
-#                          "type":"backupConfig"
-#                      },
-#                      "creation":"12h",
-#                      "extraArgs":{
-#                          "election-timeout":5000,
-#                          "heartbeat-interval":500
-#                      },
-#                      "retention":"72h",
-#                      "snapshot":false,
-#                      "type":"etcdService"
-#                  },
-#                  "kubeApi":{
-#                      "alwaysPullImages":false,
-#                      "podSecurityPolicy":false,
-#                      "serviceNodePortRange":"30000-32767",
-#                      "type":"kubeAPIService"
-#                  },
-#                  "type":"rkeConfigServices"
-#              },
-#              "sshAgentAuth":false,
-#              "type":"rancherKubernetesEngineConfig"
-#          }
-#      }' --insecure)
+#      --data-binary "{\"type\":\"cluster\",\"rancherKubernetesEngineConfig\":{\"ignoreDockerVersion\":false,\"sshAgentAuth\":false,\"type\":\"rancherKubernetesEngineConfig\",\"kubernetesVersion\":\"$kubernetes_version\",\"authentication\":{\"type\":\"authnConfig\",\"strategy\":\"x509\"},\"network\":{\"type\":\"networkConfig\",\"plugin\":\"flannel\",\"flannelNetworkProvider\":{\"iface\":\"eth1\"},\"calicoNetworkProvider\":null},\"ingress\":{\"type\":\"ingressConfig\",\"provider\":\"nginx\"},\"services\":{\"type\":\"rkeConfigServices\",\"kubeApi\":{\"podSecurityPolicy\":false,\"type\":\"kubeAPIService\"},\"etcd\":{\"type\":\"etcdService\",\"extraArgs\":{\"heartbeat-interval\":500,\"election-timeout\":5000}}}},\"name\":\"$default_cluster\"}" --insecure)
+
+CLUSTERRESPONSE=$(docker run --net host \
+    --rm \
+    $curl_prefix/curl \
+     -s 'https://127.0.0.1/v3/cluster' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" \
+     --data-binary '{
+         "type":"cluster",
+         "name":"'$default_cluster'",
+         "dockerRootDir":"/var/lib/docker",
+         "enableClusterAlerting":false,
+         "enableClusterMonitoring":false,
+         "enableNetworkPolicy":false,
+         "localClusterAuthEndpoint":{
+             "enabled":true,
+             "type":"localClusterAuthEndpoint"
+         },
+         "rancherKubernetesEngineConfig":{
+             "addonJobTimeout":30,
+             "authentication":{
+                 "strategy":"x509",
+                 "type":"authnConfig"
+             },
+             "ignoreDockerVersion":true,
+             "ingress":{
+                 "provider":"nginx",
+                 "type":"ingressConfig"
+             },
+             "kubernetesVersion":"'$kubernetes_version'",
+             "monitoring":{
+                 "provider":"metrics-server",
+                 "type":"monitoringConfig"
+             },
+             "network":{
+                 "options":{
+                     "flannel_backend_type":"vxlan"
+                 },
+                 "plugin":"canal",
+                 "type":"networkConfig"
+             },
+             "privateRegistries":[
+                 {
+                     "isDefault":true,"type":
+                     "privateRegistry",
+                     "url":""
+                 }
+             ],
+             "services":{
+                 "etcd":{
+                     "backupConfig":{
+                         "enabled":true,
+                         "intervalHours":12,
+                         "retention":6,
+                         "type":"backupConfig"
+                     },
+                     "creation":"12h",
+                     "extraArgs":{
+                         "election-timeout":5000,
+                         "heartbeat-interval":500
+                     },
+                     "retention":"72h",
+                     "snapshot":false,
+                     "type":"etcdService"
+                 },
+                 "kubeApi":{
+                     "alwaysPullImages":false,
+                     "podSecurityPolicy":false,
+                     "serviceNodePortRange":"30000-32767",
+                     "type":"kubeAPIService"
+                 },
+                 "type":"rkeConfigServices"
+             },
+             "sshAgentAuth":false,
+             "type":"rancherKubernetesEngineConfig"
+         }
+     }' --insecure)
 
 # Extract clusterid to use for generating the docker run command
 CLUSTERID=`echo $CLUSTERRESPONSE | jq -r .id`
